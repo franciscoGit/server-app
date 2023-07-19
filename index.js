120e0f57e39461e3dcbaa32fae24592a1560ceb0
Francisco Maldonado Vargas <franciscomv87@gmail.com>
2023-07-19 13:06:17 -0600

    Add reading from .env files on development

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const app = express();
 const port = 5000;
 
@@ -17,10 +21,12 @@ app.listen(port, () => {
 // custom 404
 app.use((req, res, next) => {
   res.status(404).send("Sorry can't find that!")
-})
+});
 
 // custom error handler
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send('Something broke!')
-})
+});
+
+
