15379987c055b45545212fd562448eb3cfa62cf6
Francisco Maldonado Vargas <franciscomv87@gmail.com>
2023-07-19 12:44:29 -0600

    Update .gitignore

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const port = 5000;
 
 app.use(helmet());
 
+// custom 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
+
+// custom error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname});
 });
